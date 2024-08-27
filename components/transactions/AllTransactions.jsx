1c16cca7e6a824b403fe30c456b96205c289746f
Anant Singh <38243636+anantSinghCross@users.noreply.github.com>
2024-08-27 17:11:37 +0530

    Added date range on transactions page

--- components/transactions/AllTransactions.jsx
+++ components/transactions/AllTransactions.jsx
@@ -18,7 +18,7 @@ function AllTransactions({ transactions }) {
     setTransactionTypes,
     setMinDate,
     setMaxDate,
-    setActiveCategories
+    setActiveCategories,
   } = useFilters(transactions);
   console.log(minDate, maxDate, activeCategories, transactionTypes);
 
@@ -40,14 +40,14 @@ function AllTransactions({ transactions }) {
 
   const toggleActiveCategories = (e) => {
     let selectedCategories = [];
-    const options = e.target.options
-    for(let category of options){
-      if(category.selected){
+    const options = e.target.options;
+    for (let category of options) {
+      if (category.selected) {
         selectedCategories.push(category.value);
       }
     }
     setActiveCategories(selectedCategories);
-  }
+  };
 
   const filteredTransactions = applyFilters(
     transactions,
@@ -56,12 +56,19 @@ function AllTransactions({ transactions }) {
     transactionTypes,
     activeCategories
   );
-
+  let fromDate = new Date(minDate).toDateString().split(" ").slice(1).join(" ");
+  let tillDate = new Date(maxDate).toDateString().split(" ").slice(1).join(" ");
   return (
     <section className="flex flex-col m-5 my-10 mb-28 p-5 rounded-2xl bg-white shadow-xl shadow-slate-200">
-      <Button className='fixed bottom-5 right-5 shadow-lg'>Add Expense</Button>
+      <Button className="fixed bottom-5 right-5 shadow-lg">Add Expense</Button>
       <div className="flex justify-between items-center">
-        <h3 className="mb-4 w-full font-semibold text-xl text-slate-500">Transaction History</h3>
+        <span className="flex gap-4 items-baseline">
+          <h3 className="my-3 w-full font-semibold text-xl text-slate-500">Transaction History</h3>
+          <span className="text-sm sm:text-nowrap text-slate-400">
+            Showing records from <span className="bg-slate-100 px-2 py-1 rounded text-slate-500 text-xs">{fromDate}</span> till <span className="bg-slate-100 px-2 py-1 rounded text-slate-500 text-xs">{tillDate}</span>
+          </span>
+        </span>
+
         <div className="relative">
           <Button variant="secondary" onClick={() => setOpenFilters((p) => !p)}>
             <span className="flex gap-2 items-center">
@@ -104,12 +111,11 @@ function AllTransactions({ transactions }) {
               <div className="mb-2">
                 <div className="flex justify-between items-center text-xs">
                   <h5 className=" font-bold text-xs text-slate-400 py-3">CATEGORIES</h5>
-                  {
-                    activeCategories &&
-                    activeCategories.length > 0 && (
-                      <span className=" px-2 py-1 bg-rose-50 rounded text-rose-500">{activeCategories.length}</span>
-                    )
-                  }
+                  {activeCategories && activeCategories.length > 0 && (
+                    <span className=" px-2 py-1 bg-rose-50 rounded text-rose-500">
+                      {activeCategories.length}
+                    </span>
+                  )}
                 </div>
                 <select
                   className="outline-rose-300 rounded w-full overflow-auto text-xs text-slate-700"
@@ -137,7 +143,9 @@ function AllTransactions({ transactions }) {
                 <h5 className=" font-bold text-xs text-slate-400 py-3">DURATION</h5>
 
                 <span className="pb-1 flex flex-col">
-                  <label className=" text-xs" htmlFor="min_date">From</label>
+                  <label className=" text-xs" htmlFor="min_date">
+                    From
+                  </label>
                   <input
                     className="p-1 rounded bg-slate-100"
                     id="min_date"
@@ -149,7 +157,9 @@ function AllTransactions({ transactions }) {
                 </span>
 
                 <span className="pb-1 flex flex-col">
-                  <label className=" text-xs" htmlFor="max_date">Till</label>
+                  <label className=" text-xs" htmlFor="max_date">
+                    Till
+                  </label>
                   <input
                     className="p-1 rounded bg-slate-100"
                     id="max_date"
