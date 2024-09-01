4e161f764eaa0a1977708c1f94a7eefc0ba95860
Anant Singh <38243636+anantSinghCross@users.noreply.github.com>
2024-09-01 12:53:37 +0530

    Updated git ignore

--- app/(main)/login/page.jsx
+++ app/(main)/login/page.jsx
@@ -10,19 +10,18 @@ function LoginPage() {
         <div className="rounded-2xl p-5 shadow-xl shadow-slate-200 bg-white">
           <h3 className="p-5 text-lg font-semibold text-center text-rose-400">Login</h3>
 
-          <div className="flex flex-col my-1">
-            <label className="text-sm py-2 text-slate-600" htmlFor="email">Email</label>
+          <div className="flex flex-col mb-2">
+            <label className="text-sm py-1 text-slate-600" htmlFor="email">Email</label>
             <input className=" p-2 rounded border outline-rose-400 shadow-inner" type="text" id="email" />
           </div>
 
-          <div className="flex flex-col my-1">
-            <label className="text-sm py-2 text-slate-600"  htmlFor="pwd">Password</label>
+          <div className="flex flex-col mb-2">
+            <label className="text-sm py-1 text-slate-600"  htmlFor="pwd">Password</label>
             <input className=" p-2 rounded border outline-rose-400 shadow-inner" type="password" id="pwd" />
           </div>
 
-          <div className="flex gap-2 items-center py-2">
-            <Link className="text-sm font-semibold hover:text-rose-400 hover:underline" href='/add-expense'>Go straight to adding expense</Link>
-            <Button>Login</Button>
+          <div className="flex gap-2 items-center py-2 justify-end">
+            <Button className='mt-2'>Login</Button>
           </div>
         </div>
       </div>
