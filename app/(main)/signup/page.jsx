74d05b51929a7737c09e4fda5a0c32943f14ce75
Anant Singh <38243636+anantSinghCross@users.noreply.github.com>
2024-09-15 13:57:07 +0530

    added login page

--- app/(main)/signup/page.jsx
+++ app/(main)/signup/page.jsx
@@ -2,23 +2,35 @@
 
 import Button from "@/components/ui/Button";
 import { useFormState } from "react-dom";
-import { loginUser } from "@/lib/server-actions";
+import { registerUser } from "@/lib/server-actions";
 import React from "react";
 import Alert from "@/components/ui/Alert";
 
-function LoginPage() {
-  const [state, loginUserAction] = useFormState(loginUser, null);
+function SignupPage() {
+  const [state, registerUserAction] = useFormState(registerUser, null);
   return (
     <section>
-      <form action={loginUserAction} className="flex justify-center items-center w-full">
+      <form action={registerUserAction} className="flex justify-center items-center w-full">
         <div className="rounded-2xl p-5 shadow-xl shadow-slate-200 bg-white w-full mx-5 max-w-md">
-          <h3 className="p-5 text-lg font-semibold text-center text-rose-400">Login</h3>
+          <h3 className="p-5 text-lg font-semibold text-center text-rose-400">Signup</h3>
 
           {
             state?.error && (
               <Alert variant="error">{state.error}</Alert>
             )
           }
+
+          <div className="">
+            <div className="flex flex-col mb-2 flex-1">
+              <label className="text-sm py-1 text-slate-600" htmlFor="first-name">First Name</label>
+              <input name="first-name" className=" p-2 rounded border outline-rose-400 shadow-inner" type="text" id="email" />
+            </div>
+
+            <div className="flex flex-col mb-2 flex-1">
+              <label className="text-sm py-1 text-slate-600" htmlFor="last-name">Last Name</label>
+              <input name="last-name" className=" p-2 rounded border outline-rose-400 shadow-inner" type="text" id="email" />
+            </div>
+          </div>
 
           <div className="flex flex-col mb-2">
             <label className="text-sm py-1 text-slate-600" htmlFor="email">Email</label>
@@ -36,7 +48,7 @@ function LoginPage() {
         </div>
       </form>
     </section>
-  );
+  )
 }
 
-export default LoginPage;
+export default SignupPage
